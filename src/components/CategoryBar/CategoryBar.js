2aac99ddea1c7fb6b470f551ff7f841f8b3c95ec
Changhun <wakie9292@gmail.com>
2019-01-17 19:38:29 +0900

    Mapping for menuList & ToggleEvent for Allthe menu

--- src/components/CategoryBar/CategoryBar.js
+++ src/components/CategoryBar/CategoryBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './CategoryBar.module.scss'
 const categoryBar = (props) => {
+  console.log(props.show);
   return (
     <div className = {classes.CategoryBar}>
-      <div className = {classes.All}> 모든 음식 </div>
+      <div className = {classes.All} onClick = {props.showAll}> 모든 음식 </div>
     </div>
   )
 }
--- src/containers/OrderMenu/OrderMenu.js
+++ src/containers/OrderMenu/OrderMenu.js
@@ -3,15 +3,27 @@ import React, { Component } from 'react';
 import CategoryBar from 'components/CategoryBar/CategoryBar'
 import Items from 'components/Items/Items';
 import Order from 'components/Order/Order';
+import MENU from 'menu/Menu';
 
 class OrderMenu extends Component {
+  state = {
+    MENU,
+    show : false,
+  }
+  confirmOrderHandler = () =>{
+    alert('결제하시겠습니까?');
+  }
+  
+  categoryBarHandler = () => {
+    !this.state.show ? this.setState({show : true}) : this.setState({show : false})
+  }
 
   render() {
     return(
       <>
-        <CategoryBar/>
-        <Items/>
-        <Order/>
+        <CategoryBar  showAll = {this.categoryBarHandler}/>
+        <Items show = {this.state.show} menuList = {this.state.MENU} key ="mL"/>
+        <Order ConfirmOrder = {this.confirmOrderHandler}/>
       </>
     );
   }
