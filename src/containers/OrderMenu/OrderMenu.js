2a3204fd8f5ab465890ca844f4ed0f9be1545b43
Changhun <wakie9292@gamil.com>
2019-01-30 01:32:46 +0900

    Completed Redux constructrue and all data have connected firebase by axios

--- src/containers/OrderMenu/OrderMenu.js
+++ src/containers/OrderMenu/OrderMenu.js
@@ -10,16 +10,7 @@ import * as menuDataActions from 'store/modules/menuData';
 
 class OrderMenu extends Component {
   state = {
-    controledMENU : null,
     show : false,
-    selectedMenu : [],
-    totalPrice : 0
-  }
-  setMenu = () => {
-    let setMenu = MENU.map((item) => {
-      return item;
-    })
-    this.setState({controledMENU : setMenu})
   }
   confirmOrderHandler = () =>{
     alert('결제하시겠습니까?');
@@ -30,75 +21,21 @@ class OrderMenu extends Component {
     !this.state.show ? this.setState({show : true}) : this.setState({show : false})
   }
 
-  setControledMenu = (updatedMenu,id) => {
-    const { controledMENU } = this.state;
-    const index = controledMENU.findIndex(item => item.id === id);
-    this.setState({
-      controledMENU : [
-        ...controledMENU.slice(0,index),
-        updatedMenu,
-          ...controledMENU.slice(index+1,controledMENU.length)
-      ]
-    })
-  }
-  itemCountHandler = (id, sign) => {
-    const { controledMENU } = this.state;
-    const { setControledMenu } = this;
-    const index = controledMENU.findIndex(item => item.id === id);
-    let controledItem = Object.assign({},controledMENU[index]);
-    switch(sign) {
-      case '+' :
-        controledItem.count++;
-        setControledMenu(controledItem, id);
-        break;
-      case '-' :
-        if(controledMENU[index].count !==0) 
-        controledItem.count--  
-        setControledMenu(controledItem, id);
-        break;
-      case undefined :
-        // let controledItem = Object.assign({},controledMENU[index]);
-        controledItem.count = 0;
-        setControledMenu(controledItem, id);
-        break;
-      default : 
-        break;
-    }
-  }
-  orderedItemHandler = (id) => {
-    let { controledMENU, selectedMenu, totalPrice } = this.state;
-    let arr = [...selectedMenu];
-    const index = controledMENU.findIndex(item => item.id === id);
-      if(controledMENU[index].count !== 0) {
-        let pickedItem = Object.assign({},controledMENU[index]);
-        let { count , price} = pickedItem;
-        totalPrice += count*price;
-        arr.push(pickedItem);
-        this.setState({
-          selectedMenu : arr,
-          totalPrice : totalPrice
-        })
-      }
-  }
-  componentWillMount() {
-    this.setMenu();
-  }
   componentDidMount() {
-    const { menuData, counter} = this.props;
-    console.log(menuData.toJS(), counter);
+    const { MenuDataActions } = this.props;
+    MenuDataActions.getMenuList();
   }
   render() {
     const { categoryBarHandler, confirmOrderHandler} = this;
-    const { show, totalPrice ,controledMENU, selectedMenu} = this.state; 
+    const { show} = this.state; 
+    const { menuData, selectedMenu, totalPrice } = this.props;
+    console.log('OrderMenu');
     return(
       <>
         <CategoryBar  showAll = {categoryBarHandler}/>
         <ItemsContainer 
           show = {show} 
-          controledMENU = {controledMENU} 
-          // key ="mL"
-          // selectedMenu = {selectedMenu}
-          // totalPrice = {totalPrice}
+          controledMENU = {menuData} 
         />
         <Order ConfirmOrder = {confirmOrderHandler}
           totalPrice = {totalPrice}
@@ -111,7 +48,8 @@ class OrderMenu extends Component {
 
 export default connect((state) => ({
   menuData : state.menuData.get('menu'),
-  // counter : state.menuData.get('counter')
+  selectedMenu : state.menuData.get('selectedMenu'),
+  totalPrice : state.menuData.get('totalPrice')
 }),
 (dispatch) => ({
   MenuDataActions : bindActionCreators(menuDataActions,dispatch)
