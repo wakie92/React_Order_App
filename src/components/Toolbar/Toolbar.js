91b7e55cfb578fee9fc7fb13d18cb40206ee4cd7
Changhun <wakie9292@gamil.com>
2019-02-25 15:57:06 +0900

    hamburger nav

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,8 +3,16 @@ import classes from './Toolbar.module.scss'
 const toolbar = (props) => {
   return(
     <header className = {classes.Toolbar}>
+      <nav className  = {classes.Hamburger}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </nav>
       <div className = {classes.Logo}>
         Order machine
+      </div>
+      <div>
+        
       </div>
     </header>
   )
