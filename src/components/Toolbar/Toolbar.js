39436b750d601eb832384bb10de4382249416c53
Changhun <wakie9292@gmail.com>
2019-03-11 15:05:35 +0900

    Appealing Login & Unlogin userId

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import classes from './Toolbar.module.scss'
 import Payment from 'react-icons/lib/md/payment'
+import { NavLink } from 'react-router-dom';
 const toolbar = (props) => {
+  const sty = {
+    textDecoration : 'none'
+  }
   return(
     <header className = {classes.Toolbar}>
       <nav className  = {classes.Hamburger} onClick = {(e) => {e.stopPropagation(); props.openCategory()}}>
@@ -10,9 +14,15 @@ const toolbar = (props) => {
         <div></div>
       </nav>
       <div className = {classes.Logo}>
-        Order machine
+        <NavLink exact to = '/' style = {sty}>Order machine</NavLink>
       </div>
-      <Payment className = {classes.Payment} onClick = {(e) => {e.stopPropagation(); props.openOrderListMobile()}}/>
+      <div className = { classes.User}>
+        {props.isLogined ?  
+            <span> [{props.loginUser}] </span>
+          : <span> [{props.unLoginUser}]</span>
+        }
+        <Payment className = {classes.Payment} onClick = {(e) => {e.stopPropagation(); props.openOrderListMobile()}}/>
+      </div>
     </header>
   )
 }
