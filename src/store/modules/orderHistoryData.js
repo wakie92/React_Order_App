5f9f420c840bcc46f91d6f05bfe29ca0e470a34c
Changhun Lee <wakie9292@gmail.com>
2019-04-03 12:54:56 +0900

    Resetting grid design

--- src/store/modules/orderHistoryData.js
+++ src/store/modules/orderHistoryData.js
@@ -1,11 +1,7 @@
 import { createAction, handleActions } from 'redux-actions';
 import { fromJS } from 'immutable';
-import { pender } from 'redux-pender';
-import * as api from 'lib/api';
 
-const ORDER_HISTORY = 'orderHistoryData/ORDER_HISTORY'
 const SAVE_HISTORY = 'orderHistoryData/SAVE_HISTORY'
-export const getOrderHistory = createAction(ORDER_HISTORY, api.getOrderHistory)
 export const saveHistory = createAction(SAVE_HISTORY)
 
 const initialState = fromJS({
@@ -13,19 +9,6 @@ const initialState = fromJS({
 })
 
 export default handleActions({
-  ...pender({
-    type : ORDER_HISTORY,
-    onPending : (state, action) => {
-      return console.log('[Pending] : ORDER_HISTORY');
-    },
-    onFailure : (state, action) => {
-      return console.log('[Error] : ORDER_HISTORY');
-    },
-    onSuccess : (state, action) => {
-      const orderHistoryItems = action.payload
-      return state.set('orderHistoryItems', orderHistoryItems)
-    }
-  }),
   [SAVE_HISTORY] : (state, action) => {
     const orderLog = action.payload;
     return state.set('orderHistoryItems', orderLog)
