bea369a94ecfe5af988e6d54ab8160886694410b
Changhun Lee <wakie9292@gmail.com>
2019-04-03 12:44:52 +0900

    Firebase api config

--- src/hoc/Firebase/firebase.js
+++ src/hoc/Firebase/firebase.js
@@ -1,6 +1,5 @@
 import app from 'firebase/app';
 import 'firebase/database'
-import 'firebase/auth'
 const config = {
   apiKey: "AIzaSyAVbIamQ34eNUx7XuoQvKq8CSfXJku30qU",
   authDomain: "order-app-7e078.firebaseapp.com",
@@ -12,34 +11,8 @@ class Firebase {
     constructor () {
       app.initializeApp(config);
       this.db = app.database();
-      this.auth = app.auth();
     }
-
-    //Auth API
-    docreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
-    
-    doSignInwithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password)
-    
-    doSignOut = () => this.auth.signOut();
-
-    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
-    
-    doOpenPopup = () => this.auth.signInWithPopup();
-
-    
-
     //User API
-    menu =  () => this.db.ref() 
-          .child('menu')
-          .child('1')
-          .once('value',  (data)  => {
-          }).then(  (data) =>  { 
-            const  result =   data.val();
-            return  result
-          })
-
     orderHistory = (user) =>  this.db.ref()
                           .child('orderSummary')
                           .orderByChild('os_userId')
@@ -51,7 +24,7 @@ class Firebase {
                                 userMenu.push(data.val()[item]);
                               }
                               return  userMenu;
-                            })
-    menu1 = () => this.db.ref('menu');
+                            });
+  
 }
 export default Firebase;
