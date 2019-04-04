013411794a4813cd50c6df179fa7c63ab72e24a7
Changhun Lee <wakie9292@gmail.com>
2019-04-05 04:41:57 +0900

    Order page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,25 +6,23 @@ const login = ({onLogin , isSignup, isLogined, onChangeMode, form, loginID, onLo
     <div className = {classes.LoginWrapper}>
        {
          !isLogined ?
-      <div className = {classes.LoginBox}>
-        <div className = {classes.LoginAccount}>
-          <form  className = {classes.FormWrapper} onSubmit = {onLogin}>
-            {form}
-            <Button className = {classes.LoginAccess}  btnType = 'Login'>{isSignup ? '회원가입' : '로그인'}</Button>
-          </form>
-          
-          <Button 
-                    clicked={onChangeMode}
-                    btnType="Danger">  {isSignup ? 'SIGNIN' : '회원가입'}</Button>
-        </div>
-      </div>
+          <div className = {classes.LoginBox}>
+            <div className = {classes.LoginAccount}>
+              <form  className = {classes.FormWrapper} onSubmit = {onLogin}>
+                {form}
+                <Button className = {classes.LoginAccess}  btnType = 'Login'>{ isSignup ? '회원가입' : '로그인' }</Button>
+              </form>
+              <Button clicked={onChangeMode}
+                      btnType="Danger">  {isSignup ? 'SIGNIN' : '회원가입'}</Button>
+            </div>
+          </div>
         :
-        <div  className = {classes.LoginBox}>
-          <span className  = {classes.WelcomeMessage}>
-            [{loginID}]님 로그인 되셨습니다.
-          </span>
-          <Button className = {classes.Logout} clicked = {onLogout} btnType = 'Logout'>로그아웃</Button>
-        </div>
+          <div  className = {classes.LoginBox}>
+            <span className  = {classes.WelcomeMessage}>
+              [{loginID}]님 로그인 되셨습니다.
+            </span>
+            <Button className = {classes.Logout} clicked = {onLogout} btnType = 'Logout'>로그아웃</Button>
+          </div>
       }
     </div>
   )
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -11,12 +11,16 @@ const order = (props) => {
   return (
     <>
       <div className = {attachedClasses.join(' ')} onClick = {(e) =>{e.stopPropagation();}}>
-        <div className = {classes.Header}> <span>장바구니</span></div>
+        <div className = {classes.Header}> 
+          <span>장바구니</span>
+        </div>
         <hr/>
-          <OrderListContainer/>
-          <OrderRequirement totalPrice = {props.totalPrice}/>
+        <OrderListContainer/>
+        <OrderRequirement totalPrice = {props.totalPrice}/>
         <div className = {classes.Payment} onClick = {(e) =>  e.stopPropagation()}>
-          <div className = {classes.TotalPrice}><span>주문금액</span><span>{props.totalPrice}원</span></div>
+          <div className = {classes.TotalPrice}>
+            <span>주문금액</span><span>{props.totalPrice}원</span>
+          </div>
           <hr/>
           <Button btnType = "ConfirmOrder"  clicked = {props.ConfirmOrder}>주문하기</Button>
         </div>
