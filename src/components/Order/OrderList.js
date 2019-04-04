bad99931d474e53a586d89bd8acbc8c9964edd4e
Changhun Lee <wakie9292@gmail.com>
2019-04-05 04:42:32 +0900

    Order List in OrderSection

--- src/components/Order/OrderList.js
+++ src/components/Order/OrderList.js
@@ -10,13 +10,12 @@ const orderList = ({selectedMenu,deleteItem,key}) => {
     <>
       <div  className = {classes.OrderedItems} key = {key}>
         <span className = {classes.OrderedItemName}>{item.name}</span>
-            
         <span className = {classes.OrderedItemPrice} 
               onClick = {(e) => {e.stopPropagation(); deleteItem(item.id)}} >취소</span>
         <span className = {classes.OrderedItemCount}>{item.counter}</span>
         <span className = {classes.OrderedItemTotalPrice}>{item.counter * item.price}원</span>
       </div>
-      <hr></hr>
+      <hr/>
     </>
       )
   }) 
