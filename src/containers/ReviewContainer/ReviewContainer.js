630775dfbaf6dc2a783ecdb3da6528ea3611f4cb
Changhun Lee <wakie9292@gmail.com>
2019-05-06 18:12:11 +0900

    ReviewContainer setting

--- src/containers/ReviewContainer/ReviewContainer.js
+++ src/containers/ReviewContainer/ReviewContainer.js
@@ -1,9 +1,64 @@
-import React, {useState, useEffect, useReducer} from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as orderHistoryDataActions from 'store/modules/orderHistoryData';
+import Input from 'components/UI/Input/Input';
 
-const ReviewContainer =  () => {
-  
+class ReviewContainer extends Component {
+  state = {
+    reviewForm : {
+      requirement: {
+          elementType: 'textarea',
+          elementConfig: {
+              type: 'textarea',
+              placeholder: '리뷰를 입력해 주세요',
+              name : 'req'
+          },
+          value: '',
+          validation: {
+              required: true,
+          },
+          valid: true,
+          touched: false
+      },
+    }
+  }
+
+  render() {
+    const {reviewForm} = this.state;
+    let formElements = [];
+    for (let key in reviewForm) {
+      formElements.push({
+        id : key,
+        config : reviewForm[key]
+      })
+    }
+    const form = formElements.map(formElement => (
+      <Input
+        key = {formElement.id}
+        elementType = {formElement.config.elementType}
+        elementConfig = {formElement.config.elementConfig}
+        name = {formElement.config.name}
+        // value = {this.props.review}
+        invalid = {!formElement.config.valid}
+        shouldValidate = {formElement.config.validation}
+        touched = {formElement.config.touched}
+        // changed = {(e) => }
+      />
+    ))
     return (
-      <div>sdfsdf</div>
-    )
+      <>
+        <div></div>
+        {form}
+
+      </>
+    );
+  }
 }
-export default ReviewContainer;
+
+export default connect ((state) => ({
+
+}),
+(dispatch) => ({
+  OrderHistoryDataActions : bindActionCreators(orderHistoryDataActions, dispatch)
+}))(ReviewContainer);
